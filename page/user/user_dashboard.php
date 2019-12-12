c870fc9a3f14f1fba442107dbcd4ed1c135395c3
KarolBorja <borjakarol7@gmail.com>
2019-12-12 16:22:48 +0800

    feature/add-icon-modified-user-dashboard

---
+++ page/user/user_dashboard.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../../assets/img/tab-ico.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>
+    Local Hiphop Music Channel
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <?php include '../../includes/link/css_link.php'?>
+</head>
+<body>
+	<?php include '../../includes/navbar.php' ?>
+
+	<div class="section">
+		<div class="row">
+			<!-- Latest News -->
+			<div class="container main-content col-lg-8 col-md-8" >
+				<br>
+				<br>
+				<div class="row">
+					<img src="../../assets/img/ico1.png" style="height: 50px;"><h1>atest News</h1>
+				</div>
+				<div class="card">
+	  				<img src="../../assets/img/news6.jpg" class="card-img-top " style="height: 300px;object-fit: cover;">
+  					<div class="card-body">
+    					<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  					</div>
+				</div>
+				<div class="card">
+	  				<img src="../../assets/img/news6.jpg" class="card-img-top" style="height: 300px;object-fit: cover;">
+  					<div class="card-body">
+    					<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  					</div>
+				</div>
+				<div class="card mb-3">
+	  				<img src="../../assets/img/news6.jpg" class="card-img-top" style="height: 300px;object-fit: cover;">
+  					<div class="card-body">
+    					<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  					</div>
+				</div>	
+			</div>
+			<!-- End Latest News -->
+
+			<!-- Sidebar -->
+			<div class="sidebar col-lg-3 col-md-3" id="sidebar" >
+				<!-- Popular News -->
+				<h3 align="Center">Popular News</h3>
+				<div class="card" style="width: 18rem;">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="card-link">Card link</a>
+						<a href="#" class="card-link">Another link</a>
+					</div>
+				</div>
+					<div class="card" style="width: 18rem;">
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="#" class="card-link">Card link</a>
+							<a href="#" class="card-link">Another link</a>
+						</div>
+					</div>
+				<!-- footer -->	
+				<?php include '../../includes/footer.php' ?>
+				<!-- end footer -->
+				<!-- End Popular News -->
+			</div>
+			<!-- End Sidebar -->
+		</div>
+	</div>
+    <?php include '../../includes/link/js_link.php'?>
+</body>
+</html>
