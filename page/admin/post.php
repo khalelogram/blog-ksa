4c91fa59edbd5646e7e967b964c0783c6b906771
kanutodev <rodenjakev@gmail.com>
2019-12-12 17:02:48 +0800

    added my docs file continued dev

---
+++ page/admin/post.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Post View</title>
+</head>
+<body>
+
+<div class="main container">
+	<h1>Post Title Here</h1>
+	<p style="  height: 200px; width: 50%;">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+	<div class="img container">
+		<img src="https://placekitten.com/340/250">
+		<img src="https://placekitten.com/440/250">
+	</div>
+	<div class="comment container">
+		<div>
+			<li>comment here</li>
+			<li>comment here</li>
+			<li>comment here</li>
+			<li>comment here</li>
+			<li>comment here</li>
+		</div>
+		<div>
+			<input type="text" name="comment">
+			<input type="button" name="submit" value="send">
+		</div>
+	</div>
+
+</div>
+
+</body>
+</html>
