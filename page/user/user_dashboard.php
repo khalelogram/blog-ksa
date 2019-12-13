b26c6b070705ed4631444d353f58b4ef26966d1a
KarolBorja <borjakarol7@gmail.com>
2019-12-13 15:07:07 +0800

    sidebar fixed

--- page/user/user_dashboard.php
+++ page/user/user_dashboard.php
@@ -14,72 +14,178 @@
 <body>
 	<?php include '../../includes/navbar.php' ?>
 
-	<div class="section">
-		<div class="row">
-			<!-- Latest News -->
-			<div class="container main-content col-lg-8 col-md-8" >
-				<br>
-				<br>
-				<div class="row">
-					<img src="../../assets/img/ico1.png" style="height: 50px;"><h1>atest News</h1>
-				</div>
-				<div class="card">
-	  				<img src="../../assets/img/news6.jpg" class="card-img-top " style="height: 300px;object-fit: cover;">
-  					<div class="card-body">
-    					<h5 class="card-title">Card title</h5>
-						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  					</div>
-				</div>
-				<div class="card">
-	  				<img src="../../assets/img/news6.jpg" class="card-img-top" style="height: 300px;object-fit: cover;">
-  					<div class="card-body">
-    					<h5 class="card-title">Card title</h5>
-						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  					</div>
-				</div>
-				<div class="card mb-3">
-	  				<img src="../../assets/img/news6.jpg" class="card-img-top" style="height: 300px;object-fit: cover;">
-  					<div class="card-body">
-    					<h5 class="card-title">Card title</h5>
-						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  					</div>
-				</div>	
+	<div class="section row" style="background-color: black;">
+		<!-- Latest News -->
+		<div class="container main-content col-lg-8 col-md-8 col-xs-8" style="margin-left: 50px;">
+			<br>
+			<br>
+			<div class="heading row">
+				<img src="../../assets/img/ico.png" style="height: 50px;"><h1 style="color: white;">atest News</h1>
 			</div>
-			<!-- End Latest News -->
-
-			<!-- Sidebar -->
-			<div class="sidebar col-lg-3 col-md-3" id="sidebar" >
-				<!-- Popular News -->
-				<h3 align="Center">Popular News</h3>
-				<div class="card" style="width: 18rem;">
+			<div class="card">
+				<div class="inner">
+  				<img src="../../assets/img/news6.jpg" class="card-img-top " style="height: 300px;object-fit: cover;">
+  				</div>
 					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="card-link">Card link</a>
-						<a href="#" class="card-link">Another link</a>
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. <button type="button" class="btn btn-secondary">Read more</button></p> 
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
 					</div>
-				</div>
-					<div class="card" style="width: 18rem;">
-						<div class="card-body">
-							<h5 class="card-title">Card title</h5>
-							<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							<a href="#" class="card-link">Card link</a>
-							<a href="#" class="card-link">Another link</a>
-						</div>
-					</div>
-				<!-- footer -->	
-				<?php include '../../includes/footer.php' ?>
-				<!-- end footer -->
-				<!-- End Popular News -->
 			</div>
-			<!-- End Sidebar -->
+			<div class="card">
+				<div class="inner">
+  				<img src="../../assets/img/news6.jpg" class="card-img-top" style="height: 300px;object-fit: cover;">
+  				</div>
+					<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					</div>
+			</div>
+			<div class="card">
+				<div class="inner">
+  				<img src="../../assets/img/news6.jpg" class="card-img-top" style="height: 300px;object-fit: cover;">
+  				</div>
+					<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					</div>
+			</div>
+			<div class="card">
+				<div class="inner">
+  				<img src="../../assets/img/news6.jpg" class="card-img-top" style="height: 300px;object-fit: cover;">
+  				</div>
+					<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					</div>
+			</div>
+			<button type="button" class="btn btn-secondary btn-lg btn-block">Load More</button>
 		</div>
+		<!-- End Latest News -->	
 	</div>
+
+	<!-- Sidebar -->
+	<div id="sidebar" class="w-25" style="position: absolute; right: 0; top: 0; width: 200px; padding: 10px;">
+		<!-- Popular News -->
+		<br>
+		<br>
+		<br>
+		<h3 align="Center" style="color: white;">Popular News</h3>
+		<div class="card" style="width: 18rem; margin-bottom: 15px;">
+			<div class="card-body" style="background-image: url('../../assets/img/news6.jpg');background-size: cover;">
+				<div class="container" style="background-color:rgba(255, 255, 255, 0.3)">
+				<h5 class="card-title">Card title</h5>
+				<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+				<a href="#" class="card-link">Another link</a>
+				</div>
+			</div>
+		</div>
+		<div class="card" style="width: 18rem; margin-bottom: 15px;">
+			<div class="card-body" style="background-image: url('../../assets/img/news6.jpg');background-size: cover;">
+				<div class="container" style="background-color:rgba(255, 255, 255, 0.3)">
+				<h5 class="card-title">Card title</h5>
+				<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+				<a href="#" class="card-link">Another link</a>
+				</div>
+			</div>
+		</div>
+		<div class="card" style="width: 18rem; margin-bottom: 15px;">
+			<div class="card-body" style="background-image: url('../../assets/img/news6.jpg');background-size: cover;">
+				<div class="container" style="background-color:rgba(255, 255, 255, 0.3)">
+				<h5 class="card-title">Card title</h5>
+				<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+				<a href="#" class="card-link">Another link</a>
+				</div>
+			</div>
+		</div>
+		<!-- End Popular News -->
+
+		<h3 align="Center" style="color: white;">Local Merch</h3>
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style=" margin-bottom: 15px;" >
+			<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			</ol>
+			<div class="carousel-inner">
+			<div class="carousel-item active">
+			<img src="../../assets/img/tshirt/1.1.jpg" class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+			<img src="../../assets/img/tshirt/1.2.jpg" class="d-block w-100" alt="...">
+			</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+			</a>
+		</div>
+		<!-- footer -->	
+		<?php include '../../includes/footer.php' ?>
+		<!-- end footer -->
+	</div>
+	<!-- End Sidebar -->
+
     <?php include '../../includes/link/js_link.php'?>
+
+<script>
+			function fixMe(id) {
+		    var e = $(id);
+		    var lastScrollTop = 0;
+		    var firstOffset = e.offset().top;
+		    var lastA = e.offset().top;
+		    var isFixed = false;
+		    $(window).scroll(function(event){
+		        if (isFixed) {
+		            return;
+		        }
+		        var a = e.offset().top;
+		        var b = e.height();
+		        var c = $(window).height();
+		        var d = $(window).scrollTop();
+		        if (b <= c - a) {
+		            e.css({position: "fixed"});
+		            isFixed = true;
+		            return;
+		        }           
+		        if (d > lastScrollTop){ // scroll down
+		            if (e.css("position") != "fixed" && c + d >= a + b) {
+		                e.css({position: "fixed", bottom: 0, top: "auto"});
+		            }
+		            if (a - d >= firstOffset) {
+		                e.css({position: "absolute", bottom: "auto", top: lastA});
+		            }
+		        } else { // scroll up
+		            if (a - d >= firstOffset) {
+		                if (e.css("position") != "fixed") {
+		                    e.css({position: "fixed", bottom: "auto", top: firstOffset});
+		                }
+		            } else {
+		                if (e.css("position") != "absolute") {
+		                    e.css({position: "absolute", bottom: "auto", top: lastA});
+		                }               
+		            }
+		        }
+		        lastScrollTop = d;
+		        lastA = a;
+		    });
+		}
+		fixMe("#sidebar");
+</script>
+
+
 </body>
 </html>
